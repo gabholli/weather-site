fc1613589a5847c9e836ac1b148cd70d22308454
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2023-12-30 02:49:18 -0800

    Added border to form elements

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,14 +6,17 @@ const Form = ({ handleSubmit }) => {
             <div>
                 <form className="flex gap-6" onSubmit={handleSubmit}>
                     <input
-                        className="px-2 py-1 md:px-4 md:py-2 text-sm rounded"
+                        className="px-2 py-1 md:px-4 md:py-2 text-sm rounded border-gray-800 border-2"
                         type="text"
                         // value={cityName || ""}
                         placeholder="Enter city name"
                         name="search"
                     // onChange={event => handleSubmit(event.target.value)}
                     ></input>
-                    <button className="bg-gray-300 px-2 py-1 md:px-4 md:py-2 text-sm rounded">Search</button>
+                    <button className="bg-gray-300 hover:bg-gray-500 active:bg-gray-600 
+                        px-2 py-1 md:px-4 md:py-2 text-sm rounded border-gray-800 border-2"
+                    >Search
+                    </button>
                 </form>
             </div>
         </header>
